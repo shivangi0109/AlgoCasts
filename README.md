1a7c9dfd7463d412bac38ca2d2e3f5cdb1977193
Shivangi Srivastava <shi06dec@gmail.com>
2024-07-31 17:51:07 -0600

    Reverse a string

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # AlgoCasts
 
 Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
+
+# How to run the jest test
+No need to start vagrant - For running test runt the command like this - npx jest fib/test.js --watch
+
+For debugging - use debugger; inside the function and make sure you call the function. 
+Then stop the test and cd into the exercises folder then run node inspect fib/index.js
+debug>cont or c
+debug>repl
+>str
+> copy the code and see the result in repl mode
+> ctrl + c
+debug>c
+debug>ctrl + c
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,54 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // Solution 1 -
+
+  // return str.split('').reverse().join('');
+
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+
+  // Solution 2 -
+
+  // let reversed = "";
+
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reversed += str[i];
+  // }
+  // OR
+  // for (let character of str) {
+  //   reversed = character + reversed;
+  // }
+  // return reversed;
+
+  // The concatenation operation itself takes O(k) time, where k is the current length of the reversed string.
+  // However, in JavaScript, string concatenation inside a loop is generally O(n) because it creates a new string each time.
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(n)
+
+  // Solution 3 -
+  // return str.split('').reduce((reversed, character) => {
+  //   return character + reversed;
+  // }, '');
+
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(n)
+
+  // Solution 4 - 
+
+  let reversedArray = [];
+  
+  // Iterate over the string from the end to the beginning
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedArray.push(str[i]);
+  }
+  
+  // Join the array into a string
+  return reversedArray.join('');
+
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+}
 
 module.exports = reverse;
